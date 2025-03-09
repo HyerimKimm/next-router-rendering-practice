82c8a0f260ac05e9b9d0767ed8972c2b81d4f2fa
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-03-09 18:03:40 +0900

    Feat. Not Found 페이지 추가

---
+++ components/error/Error.tsx
@@ -0,0 +1,25 @@
+import { CSSProperties } from "react";
+import styles from "./Error.module.css";
+
+type ErrorComponentPropsType = {
+  code: number;
+  title: string;
+  message: string;
+  style?: CSSProperties;
+};
+
+export default function ErrorComponent({
+  code,
+  title,
+  message,
+  style,
+}: ErrorComponentPropsType) {
+  return (
+    <div className={styles.error} style={style}>
+      <h1>
+        ⚠️ {code} {title}
+      </h1>
+      <p>{message}</p>
+    </div>
+  );
+}
