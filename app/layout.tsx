a22b27c52d01dc6bfbe48988173bd463a4092c06
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-03-17 22:36:01 +0900

    Feat.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import MainHeader from "@/components/main_header/MainHeader";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <MainHeader />
-      {children}
-    </>
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
   );
 }
